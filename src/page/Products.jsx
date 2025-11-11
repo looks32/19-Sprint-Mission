05878b5a3319c6d61fa84385db60ee176fc92713
Dang <wheodnjs2@naver.com>
2025-11-11 17:04:33 +0900

    상품상세 api연결

--- src/page/Products.jsx
+++ src/page/Products.jsx
@@ -1,10 +1,26 @@
 import styled from 'styled-components';
 import ProductDesc from '../components/ProductDesc';
+import { useEffect, useState } from 'react';
+import { getProduct } from '../utill/api';
+import { useLocation, useParams } from 'react-router-dom';
 
 function Products() {
+  const [product, setProduct] = useState();
+
+  const { productId } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getProduct(productId);
+      setProduct(res);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <ProductsWrap>
-      <ProductDesc />
+      <ProductDesc {...product} />
     </ProductsWrap>
   );
 }
--- src/utill/api.js
+++ src/utill/api.js
@@ -10,3 +10,12 @@ export async function getProducts(page, pageSize, orderBy) {
   }
   return res.json();
 }
+
+export async function getProduct(id) {
+  const res = await fetch(`${BASE_URL}/products/${id}`);
+  if (!res.ok) {
+    const text = await res.text();
+    throw new Error(`${text}`);
+  }
+  return res.json();
+}
