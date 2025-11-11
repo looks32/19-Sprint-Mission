81a9a168458de38d9172ad6b9c1808bc80f6df7d
Dang <wheodnjs2@naver.com>
2025-11-11 17:15:23 +0900

    상품 상세 내용 수정

--- src/components/ProductDesc.jsx
+++ src/components/ProductDesc.jsx
@@ -1,27 +1,30 @@
 import styled from 'styled-components';
 import TagList from './TagList';
 
-function ProductDesc() {
+function ProductDesc({
+  id,
+  name,
+  description,
+  price,
+  tags,
+  images,
+  favoriteCount,
+}) {
   return (
     <ProductDescWrap>
       <ImgArea>
-        <img src="/aa.png" alt="" />
+        <img src={images} alt={name} />
       </ImgArea>
       <TextArea>
         <div className="title">
-          <h2>아이패드미니 팔아요</h2>
-          <strong>500,000원</strong>
+          <h2>{name}</h2>
+          <strong>{price?.toLocaleString('ko-KR')}원</strong>
         </div>
         <div className="detail">
           <strong>상품 소개</strong>
-          <p>
-            액정에 잔기스랑 주변부 스크래치있습니다만 예민하신분아니면 전혀
-            신경쓰이지않을정도입니다. 박스 보관중입니다. 메모용과
-            넷플릭스용으로만쓰던거라 뭘 해보질 않아 기능이나 문제점을 못느꼈네요
-            잘 안써서 싸게넘깁니다! 택배거래안합니다.
-          </p>
+          <p>{description}</p>
           <strong>상품 태그</strong>
-          <TagList />
+          <TagList tags={tags} />
           <div>프로필 하트</div>
         </div>
       </TextArea>
