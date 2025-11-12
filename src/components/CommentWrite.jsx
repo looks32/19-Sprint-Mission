534007081cf6bed020b88b5fe68933b6d6e2f564
Dang <wheodnjs2@naver.com>
2025-11-12 16:33:23 +0900

    댓글 작성 컴포넌트 생성

---
+++ src/components/CommentWrite.jsx
@@ -0,0 +1,16 @@
+import Textarea from '../components/Textarea';
+import Button from './Button';
+
+function CommentWrite() {
+  return (
+    <div>
+      <Textarea
+        height={104}
+        placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
+      />
+      <Button buttonName="등록" disabled={true} />
+    </div>
+  );
+}
+
+export default CommentWrite;
